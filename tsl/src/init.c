b4532568dab8f51ed3523ff02786efb918d4cfa5
Matvey Arye <mat@timescale.com>
2018-06-07 15:51:07 -0400

    Add remote txns support (1pc)
    This commit adds the ability to open remote transactions
    that share transactional semantics with the ongoing local
    transaction. This is done by adding txn hooks to the local
    transaction that performs txn commands on remote nodes.
    
    This commit adds the 1pc variant of the commit protocol.
    A 2-pc variant is going to be added in an upcoming PR.
    The 1pc variant exists mostly for performance testing (i.e. how much is the 2pc penalty).
    But, there are also cases where 1pc is good enough (non-critical
    data and/or data backed by Kafka).

--- tsl/src/init.c
+++ tsl/src/init.c
@@ -27,6 +27,7 @@
 
 #if !PG96
 #include "remote/connection_cache.h"
+#include "remote/dist_txn.h"
 #endif
 
 #ifdef PG_MODULE_MAGIC
@@ -111,6 +112,7 @@ ts_module_init(PG_FUNCTION_ARGS)
 
 #if !PG96
 	_remote_connection_cache_init();
+	_remote_dist_txn_init();
 #endif
 
 	PG_RETURN_BOOL(true);
@@ -128,6 +130,7 @@ module_shutdown(void)
 	 * document any exceptions.
 	 */
 #if !PG96
+	_remote_dist_txn_fini();
 	_remote_connection_cache_fini();
 #endif
 
