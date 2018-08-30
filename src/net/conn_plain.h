98c67e09ef6144723807c5b5d32d0cb2d8383c5d
Erik Nordström <erik@timescale.com>
2018-08-30 15:17:23 +0200

    Add full path to header defines
    The initial #ifndef in header files, which protects against including
    headers multiple times, should include the full path in the prefix so
    that identically named files in other paths do not conflict.
    
    This change fixes the paths for some existing header files.

--- src/net/conn_plain.h
+++ src/net/conn_plain.h
@@ -1,9 +1,9 @@
-#ifndef TIMESCALEDB_CONN_PLAIN_H
-#define TIMESCALEDB_CONN_PLAIN_H
+#ifndef TIMESCALEDB_NET_CONN_PLAIN_H
+#define TIMESCALEDB_NET_CONN_PLAIN_H
 
 typedef struct Connection Connection;
 
 extern int	plain_connect(Connection *conn, const char *host, int port);
 extern void plain_close(Connection *conn);
 
-#endif							/* TIMESCALEDB_CONN_PLAIN_H */
+#endif							/* TIMESCALEDB_NET_CONN_PLAIN_H */
--- test/src/net/conn_mock.h
+++ test/src/net/conn_mock.h
@@ -1,5 +1,5 @@
-#ifndef TIMESCALEDB_CONN_MOCK_H
-#define TIMESCALEDB_CONN_MOCK_H
+#ifndef TIMESCALEDB_TEST_NET_CONN_MOCK_H
+#define TIMESCALEDB_TEST_NET_CONN_MOCK_H
 
 #include <sys/socket.h>
 
@@ -7,4 +7,4 @@ typedef struct Connection Connection;
 
 extern ssize_t connection_mock_set_recv_buf(Connection *conn, char *buf, size_t buflen);
 
-#endif							/* TIMESCALEDB_CONN_MOCK_H */
+#endif							/* TIMESCALEDB_TEST_NET_CONN_MOCK_H */
