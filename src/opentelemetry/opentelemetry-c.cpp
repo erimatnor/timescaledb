d680604e542986227ea60c7d24ebea25da82c95a
Erik Nordström <erik@timescale.com>
2022-10-25 13:26:46 +0200

    Fixes for MacOS and more APIs wrapped

--- src/opentelemetry/opentelemetry-c.cpp
+++ src/opentelemetry/opentelemetry-c.cpp
@@ -7,8 +7,86 @@
 #include <opentelemetry/trace/provider.h>
 #include "opentelemetry-c.h"
 
-TracerProvider *
-ts_opentelemetry_get_provider(void)
+struct TracerProvider {
+    opentelemetry::nostd::shared_ptr<opentelemetry::trace::TracerProvider> provider;
+};
+
+struct Tracer {
+    opentelemetry::nostd::shared_ptr<opentelemetry::trace::Tracer> tracer;
+};
+
+struct Span {
+    opentelemetry::nostd::shared_ptr<opentelemetry::trace::Span> span;
+};
+
+struct Scope {
+    opentelemetry::trace::Scope scope;
+};
+
+struct TracerProvider *
+ts_opentelemetry_provider_get(void)
 {
-	return trace::Provider::GetTracerProvider().get();
+    struct TracerProvider *p = static_cast<struct TracerProvider *>(malloc(sizeof(struct TracerProvider)));    
+    p->provider = opentelemetry::trace::Provider::GetTracerProvider();
+    return p;
 }
+
+void
+ts_opentelemetry_tracer_provider_delete(struct TracerProvider *tp)
+{
+    delete &tp->provider;
+    free(tp);
+}
+    
+struct Tracer *
+ts_opentelemetry_tracer_get(struct TracerProvider *tp, const char *library_name, const char *version)
+{
+    struct Tracer *t = static_cast<struct Tracer *>(malloc(sizeof(struct Tracer)));
+    t->tracer = tp->provider->GetTracer(library_name, version);
+    return t;
+}
+
+void
+ts_opentelemetry_tracer_delete(struct Tracer *t)
+{
+    delete &t->tracer;
+    free(t);
+}
+
+struct Span *
+ts_opentelemetry_span_start(struct Tracer *t, const char *name)
+{
+    struct Span *s = static_cast<struct Span *>(malloc(sizeof(struct Span)));
+    s->span = t->tracer->StartSpan(name);
+    return s;
+}
+
+void
+ts_opentelemetry_span_end(struct Span *s)
+{
+    s->span->End();
+}
+
+
+void
+ts_opentelemetry_span_delete(struct Span *s)
+{
+    delete &s->span;
+    free(s);
+}
+
+struct Scope *
+ts_opentelemetry_tracer_with_active_span(struct Tracer *t, struct Span *s)
+{
+    struct Scope *sc = static_cast<struct Scope *>(malloc(sizeof(struct Scope)));
+    sc->scope = t->tracer->WithActiveSpan(s->span);
+    return sc;
+}
+
+void
+ts_opentelemetry_scope_delete(struct Scope *sc)
+{
+    delete &sc->scope;
+    free(sc);
+}
+
--- src/opentelemetry/opentelemetry-c.h
+++ src/opentelemetry/opentelemetry-c.h
@@ -9,11 +9,24 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+	struct TracerProvider;
+	struct Tracer;
+	struct Span;
+	struct Scope;
 	
+	struct TracerProvider *ts_opentelemetry_provider_get();
+	void ts_opentelemetry_tracer_provider_delete(struct TracerProvider *tp);
+	
+	struct Tracer *ts_opentelemetry_tracer_get(struct TracerProvider *tp, const char *library_name, const char *version);
+    void ts_opentelemetry_tracer_delete(struct Tracer *t);
 
-	TracerProvider *ts_opentelemetry_get_provider();
-    
+	struct Span *ts_opentelemetry_span_start(struct Tracer *t);
+	void ts_opentelemetry_span_end(struct Span *s);
+	void ts_opentelemetry_span_delete(struct Span *s);
+
+	struct Scope *ts_opentelemetry_tracer_with_active_span(struct Tracer *t, struct Span *s);	
+	void ts_opentelemetry_scope_delete(struct Scope *sc);
+	
 #ifdef __cplusplus
 }
 #endif
