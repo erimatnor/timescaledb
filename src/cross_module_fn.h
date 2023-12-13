4f90e59cd57355163ff28713918e0674a4d7eeaa
Erik Nordström <erik@timescale.com>
2023-12-13 10:00:45 +0100

    Fix issue when converting from compression TAM
    When converting a table from compression TAM to another TAM (typically
    heap), it is necessary to truncate the compressed relation since all
    its data is written into the new heap (in uncompressed form). If no
    truncation occurs, the data will be duplicated since, after converting
    back to heap, the table is technically still compressed in the "old"
    way where data is also read from the compressed relation.
    
    Ideally, converting back to heap from compression TAM should mean one
    of two things:
    
    - the table/chunk is no longer using compression, so the compressed
      relation should be removed and all data put (uncompressed) in the
      main heap.
    
    - the table/chunk is still compressed and no data is
      re-written/converted. The only change is that it is no longer using
      compression TAM.
    
    However, one of those behaviors will have to be implemented in a
    future change.
    
    This change also disables the optimization to provide ordered sequence
    scans in some cases since it doesn't (yet) produce the right order
    when there is data in both the compressed and non-compressed
    relations.

--- src/cross_module_fn.h
+++ src/cross_module_fn.h
@@ -90,6 +90,7 @@ typedef struct CrossModuleFunctions
 	PGFunction move_chunk;
 
 	void (*ddl_command_start)(ProcessUtilityArgs *args);
+	void (*ddl_command_end)(EventTriggerData *trigdata);
 
 	/* Vectorized queries */
 	bool (*push_down_aggregation)(PlannerInfo *root, AggPath *aggregation_path, Path *subpath);
