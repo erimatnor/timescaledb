09ac0f8a5d2d3e408afdf4a8df3f8c5c415d8755
Erik Nordström <erik@timescale.com>
2025-05-27 13:27:13 +0200

    Add concurrent mode for merging chunks
    Add an option to merge chunks in concurrent mode, allowing concurrent
    transactions to query and execute modifications on the hypertable while
    chunks are being merged. Any modifying operations on the chunks being
    merged (e.g., inserts) are still blocked.
    
    Example of a concurrent merge of two chunks:
    
    ```sql
    call merge_chunks('chunk1', 'chunk2', concurrently => true);
    ```
    
    The concurrent mode runs across two transactions. The first transaction
    locks the chunks to merge in ExclusiveLock mode. Then it writes the
    chunks' data to a new (temporary) relation, rebuilds indexes, and then
    commits. Just before committing, the merge takes session locks on all
    chunks to protect them against changes across the two transactions. The next
    transaction takes local locks on all chunks (including the temporary
    merged relations) in AccessExclusiveLock mode and completes the merge by
    swapping the heaps on the result chunk and its indexes.
    
    While concurrent mode technically does a lock upgrade from ExclusiveLock
    to AccessExclusiveLock (via session locks), it still prevents a number
    of deadlock scenarios because all other locks are released when the first
    transaction commits. Since no other transaction can acquire a
    conflicting lock while the merge holds the session locks, and the merge
    holds no other locks, it can simply wait until the AccessExclusiveLock
    is available.
    
    A merge could fail in the second transaction and leave behind the
    rewritten heaps for the merged relation. To handle such failures, the
    merge records the merge relations and temporary heaps in a new catalog
    table `chunk_rewrite`. If a merge command detects a previously failed
    merge via this catalog table, it will clean up any leftover heaps. An
    explicit cleanup procedure is also provided to clean up all failed
    merges.

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,2 +1,10 @@
 DROP FUNCTION IF EXISTS _timescaledb_functions.policy_job_stat_history_retention;
 DROP VIEW IF EXISTS timescaledb_information.chunks;
+
+-- Revert support for concurrent merge chunks()
+DROP PROCEDURE IF EXISTS _timescaledb_functions.chunk_rewrite_cleanup();
+DROP PROCEDURE IF EXISTS @extschema@.merge_chunks_concurrently(REGCLASS[]);
+DROP PROCEDURE IF EXISTS @extschema@.merge_chunks(REGCLASS, REGCLASS, BOOLEAN);
+ALTER EXTENSION timescaledb DROP TABLE _timescaledb_catalog.chunk_rewrite;
+DROP TABLE IF EXISTS _timescaledb_catalog.chunk_rewrite;
+
