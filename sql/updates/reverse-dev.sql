4e47c55e8cc2463eb2556911b1f2a50222a4c67d
David Beck <dbeck@timescale.com>
2025-07-14 17:19:10 +0100

    Add UUIDv7 functions
    I add a few UUIDv7 related functions so we don't depend on
    3rd party extensions for using UUID v7 in timescaledb. This
    is very useful for testing the UUID compression across pre
    PG v18.

---
+++ sql/updates/reverse-dev.sql
@@ -0,0 +1,5 @@
+DROP FUNCTION IF EXISTS _timescaledb_functions.generate_uuid_v7;
+DROP FUNCTION IF EXISTS _timescaledb_functions.uuid_v7_from_timestamptz;
+DROP FUNCTION IF EXISTS _timescaledb_functions.timestamptz_from_uuid_v7;
+DROP FUNCTION IF EXISTS _timescaledb_functions.uuid_version;
+
