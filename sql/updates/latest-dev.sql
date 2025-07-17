11cb774a2e956234f15362d0ccdfcdaab2c6ea90
David Beck <dbeck@timescale.com>
2025-07-17 11:11:25 +0100

    Add support for UUID compression
    This change introduces a new compression algorithm to support
    compressing UUIDs. This is only effective for UUID v7 type where
    the timestamp part is compressible and the random part is not.
    
    The new compression reverts to dictionary compression if it detects
    redundant data and it estimates that dictionary compression provides
    better results. Note that dictionary compression reverts to Array
    compression if it thinks it results in better compression.
    
    The new feature is controlled by a feature flag, which is turned
    off for compatibility reasons:
    
      `timescaledb.enable_uuid_compression=off`
    
    This will be shipped as off for one release and then changed to
    `on` in subsequent releases.
    
    The UUID compression divides the UUID into two parts. The compressible
    part is compressed with the delta-delta compression, and the random part
    is stored as a sequence of uint64s.
    
    This change supports the row-based decompression. The bulk decompression
    and vectorised filtering will come in a separate change.

---
+++ sql/updates/latest-dev.sql
@@ -0,0 +1,3 @@
+INSERT INTO _timescaledb_catalog.compression_algorithm( id, version, name, description) values
+( 7, 1, 'COMPRESSION_ALGORITHM_UUID', 'uuid');
+
--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -3,3 +3,5 @@ DROP FUNCTION IF EXISTS _timescaledb_functions.uuid_v7_from_timestamptz;
 DROP FUNCTION IF EXISTS _timescaledb_functions.timestamptz_from_uuid_v7;
 DROP FUNCTION IF EXISTS _timescaledb_functions.uuid_version;
 
+DELETE FROM _timescaledb_catalog.compression_algorithm WHERE id = 7 AND version = 1 AND name = 'COMPRESSION_ALGORITHM_UUID';
+
