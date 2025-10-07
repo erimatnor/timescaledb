19fceaabb219c19bdfe3e76304cb15bd820b44d8
Erik Nordström <erik@tigerdata.com>
2025-10-07 17:35:27 +0200

    Remove pending_merge field

--- src/ts_catalog/catalog.h
+++ src/ts_catalog/catalog.h
@@ -374,7 +374,6 @@ enum Anum_chunk
 	Anum_chunk_status,
 	Anum_chunk_osm_chunk,
 	Anum_chunk_creation_time,
-	Anum_chunk_pending_merge_oid,
 	_Anum_chunk_max,
 };
 
@@ -391,7 +390,6 @@ typedef struct FormData_chunk
 	int32 status;
 	bool osm_chunk;
 	TimestampTz creation_time;
-	Oid pending_merge_oid;
 } FormData_chunk;
 
 typedef FormData_chunk *Form_chunk;
