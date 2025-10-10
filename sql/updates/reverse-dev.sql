3defc6bb4cc6dfd5986d242827a37a6ccf47a5a4
Erik Nordström <erik@tigerdata.com>
2025-10-10 12:05:01 +0200

    Tweak downgrade script

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -14,7 +14,7 @@ DROP VIEW IF EXISTS timescaledb_information.job_stats;
 
 -- Revert support for concurrent merge chunks()
 DROP PROCEDURE IF EXISTS _timescaledb_functions.chunk_rewrite_cleanup();
-DROP PROCEDURE IF EXISTS @extschema@.merge_chunks_concurrently();
+DROP PROCEDURE IF EXISTS @extschema@.merge_chunks_concurrently(REGCLASS[]);
 DROP PROCEDURE IF EXISTS @extschema@.merge_chunks(REGCLASS, REGCLASS, BOOLEAN);
 ALTER EXTENSION timescaledb DROP TABLE _timescaledb_catalog.chunk_rewrite;
 DROP TABLE IF EXISTS _timescaledb_catalog.chunk_rewrite;
